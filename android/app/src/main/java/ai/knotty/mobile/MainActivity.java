b3fb7669e261759a10f642a7c33fea96a2392017
clean128 <markus.niemela27@gmail.com>
2025-06-17 06:54:03 +0900

    Add capacitor config

---
+++ android/app/src/main/java/ai/knotty/mobile/MainActivity.java
@@ -0,0 +1,5 @@
+package ai.knotty.mobile;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "ai.knotty.mobile",
+  appName: "Knotty Ai",
+  webDir: "dist",
+};
+
+export default config;
